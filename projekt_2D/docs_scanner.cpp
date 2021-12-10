3f3c6dc3df782d278e6f5e602911127896b4887a
VenetianDevil <rolcia07@gmail.com>
2021-12-10 19:06:59 +0100

    przemianowane lekko, koniec?

--- projekt_2D/docs_scanner.cpp
+++ projekt_2D/docs_scanner.cpp
@@ -11,26 +11,21 @@ using namespace std;
 //  g++ -O docs_scanner.cpp `pkg-config opencv --cflags --libs`
 ///////////////  Project 2 - Document Scanner  //////////////////////
 
-Mat imgOriginal, imgGray, imgBlur, imgCanny, imgThre, imgDil, imgErode, imgWarp, imgCrop;
-VERTICES initialPoints,docPoints;
+VERTICES initialPoints, docPoints;
 float w = 420, h = 596;
 
-Mat preProcessing(Mat img)
-{
-	cvtColor(img, imgGray, COLOR_BGR2GRAY);
-  // imwrite("gray.png", imgGray);
-	GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
-  // imwrite("blur.png", imgBlur);
+Mat preProcessing(Mat img) {
+  Mat imgPrecessed;
 
-	Canny(imgBlur, imgCanny, 25, 150);
-  // imwrite("canny.png", imgCanny);
+	cvtColor(img, imgPrecessed, COLOR_BGR2GRAY);
+	GaussianBlur(imgPrecessed, imgPrecessed, Size(3, 3), 3, 0);
+
+	Canny(imgPrecessed, imgPrecessed, 25, 150);
 	
   Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
-	dilate(imgCanny, imgDil, kernel);
-  imwrite("dil.png", imgDil);
+	dilate(imgPrecessed, imgPrecessed, kernel);
 
-	//erode(imgDil, imgErode, kernel);
-	return imgDil;
+	return imgPrecessed;
 }
 
 bool shapesComparator(const VERTICES & s1, const VERTICES & s2) {
@@ -47,34 +42,19 @@ VERTICES getContours(Mat image) {
 	vector<Vec4i> hierarchy;
 
 	findContours(image, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	//drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
-	// vector<VERTICES> conPoly(contours.size());
 	vector<Rect> boundRect(contours.size());
-
-	VERTICES biggest;
-	int maxArea=0;
 
   VERTICES biggestContour = *max_element(begin(contours), end(contours), shapesComparator);
 
   float peri = arcLength(biggestContour, true);
   VERTICES conPoly;
 	approxPolyDP(biggestContour, conPoly, 0.02 * peri, true);
-	biggest = { conPoly[0], conPoly[1], conPoly[2], conPoly[3] };
+	VERTICES biggest = { conPoly[0], conPoly[1], conPoly[2], conPoly[3] };
 
 	return biggest;
 }
 
-void drawPoints(VERTICES points, Scalar color)
-{
-	for (int i = 0; i < points.size(); i++)
-	{
-		circle(imgOriginal, points[i], 10, color, FILLED);
-		putText(imgOriginal, to_string(i), points[i], FONT_HERSHEY_PLAIN, 4, color, 4);
-	}
-}
-
-VERTICES reorder(VERTICES points)
-{
+VERTICES reorder(VERTICES points) {
 	VERTICES newPoints;
 	vector<int> sumPoints, subPoints;
 
@@ -92,7 +72,7 @@ VERTICES reorder(VERTICES points)
 	return newPoints;
 }
 
-bool isOrientationHorizontal(VERTICES points){
+bool isOrientationHorizontal(VERTICES points) {
   double vec10 = sqrt(pow(points[0].x - points[1].x, 2) + pow(points[0].y - points[1].y, 2));
   double vec32 = sqrt(pow(points[2].x - points[3].x, 2) + pow(points[2].y - points[3].y, 2));
   double vec20 = sqrt(pow(points[0].x - points[2].x, 2) + pow(points[0].y - points[2].y, 2));
@@ -103,32 +83,54 @@ bool isOrientationHorizontal(VERTICES points){
 
 Mat getWarp(Mat img, VERTICES points, float w, float h )
 {
-	Point2f src[4] = { points[0], points[1], points[2], points[3] };
-	Point2f dst[4] = { {0.0f,0.0f},{w,0.0f},{0.0f,h},{w,h} };
+  Mat imgWarp;
 
-	Mat matrix = getPerspectiveTransform(src, dst);
-	warpPerspective(img, imgWarp, matrix, Point(w, h));
+	Point2f src[4] = { points[0], points[1], points[2], points[3] }; // znalezione rogi kartki
+	Point2f dst[4] = { {0.0f,0.0f},{w,0.0f},{0.0f,h},{w,h} }; // docelowe rogi zdjęcia 00. w0, 0h, wh
+
+	Mat matrix = getPerspectiveTransform(src, dst); //kalkuluje perspektywe
+	warpPerspective(img, imgWarp, matrix, Point(w, h)); //"zwija" prostuje znalezione punkty kartki do rogów pliku
 
 	return imgWarp;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 
+  Mat imgOriginal;
+
+  if(argc < 2){
+    cout << "Nie podano ścieżki do pliku" << endl;
+    return -1;
+  }
+
+  if(argc == 2){
+    try{
+      imgOriginal = imread(argv[1]);
+    } catch( Exception& e ){
+      cout << "Problem z odczytem pliku" << endl;
+      cout << e.what() << endl;
+      return -1;
+    }
+  }
+
+  if (imgOriginal.empty()) {
+    cout << "Nie ma takiego pliku" << endl;
+    return -1;
+  }
 	// string path = "Resources/paper.jpg";
-	// string path = "Resources/zolta_kartka.jpg";
+	// string path = "Resources/reklama.jpg";
 	// string path = "Resources/kara.jpg";
-	string path = "Resources/trees.jpg";
-	imgOriginal = imread(path);
+	// string path = "Resources/trees.jpg";
+
+	// imgOriginal = imread(path);
 	//resize(imgOriginal, imgOriginal, Size(), 0.5, 0.5);
 
 	// Preprpcessing - Step 1 
-	imgThre = preProcessing(imgOriginal);
+	Mat imgPrecessed = preProcessing(imgOriginal);
 
 	// Get Contours - Biggest  - Step 2
-	initialPoints = getContours(imgThre);
-	//drawPoints(initialPoints, Scalar(0, 0, 255));
+	initialPoints = getContours(imgPrecessed);
 	docPoints = reorder(initialPoints);
-	//drawPoints(docPoints, Scalar(0, 255, 0));
 
   // swap width and height if paper is positioned horizontaly
   if(isOrientationHorizontal(docPoints)){
@@ -138,17 +140,15 @@ int main() {
   }
 
 	// Warp - Step 3 
-	imgWarp = getWarp(imgOriginal, docPoints, w, h);
+	Mat imgWarp = getWarp(imgOriginal, docPoints, w, h);
 
 	//Crop - Step 4
 	int cropVal= 5;
 	Rect roi(cropVal, cropVal, w - (2 * cropVal), h - (2 * cropVal));
-	imgCrop = imgWarp(roi);
+	Mat imgFinal = imgWarp(roi);
 
-	imwrite("org.png", imgOriginal);
-	//imshow("Image Dilation", imgThre);
-	//imshow("Image Warp", imgWarp);
-	imwrite("new.png", imgCrop);
+	// imwrite("org.png", imgOriginal);
+	imwrite("new.png", imgFinal);
 	waitKey(0);
 
 }
